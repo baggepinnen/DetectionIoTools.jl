52f622c93ac9d454b1bbedc97cf962f8e963cb44
Fredrik Bagge Carlson <cont-frb@ulund.org>
2020-05-16 14:19:40 +0200

    initial commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,12 @@ using DetectionIoTools
 using Test
 
 @testset "DetectionIoTools.jl" begin
-    # Write your tests here.
+    @testset "Save interesting" begin
+        @info "Testing Save interesting"
+        allsounds = [randn(100) for _ in 1:10]
+
+        sounddir = save_interesting(allsounds, [1,3])
+        @test readdir(sounddir) |> length == 3
+
+    end
 end
